2b6357e0c84b6e3e9e15cbbe1f52bcaf3b823dcd
Emir Bozer <emir.bozer@gmail.com>
2016-11-14 23:56:25 -0500

    main menu w/sprites

---
+++ Assets/Scripts/HawkController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HawkController : MonoBehaviour {
+
+    private Transform _target;
+    public float moveSpeed; 
+    private int rotationSpeed = 3;
+
+
+    void Start()
+    {
+        _target = GameObject.FindWithTag("player").transform;
+    }
+
+
+    void FixedUpdate()
+    {
+        //rotate to look at the player
+        transform.rotation = Quaternion.Slerp(transform.rotation,
+        Quaternion.LookRotation(_target.position - transform.position), rotationSpeed * Time.deltaTime);
+
+        //move towards the player
+        GetComponent<Rigidbody>().AddForce(transform.forward * moveSpeed * Time.deltaTime);
+    }
+}

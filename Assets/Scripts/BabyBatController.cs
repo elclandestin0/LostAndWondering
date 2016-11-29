f298ef3bda9a8d4d726a77bf01ed1fe1d8188bc9
Emir Bozer <emir.bozer@gmail.com>
2016-11-29 02:35:29 -0500

    Hud integrated. baby status,hawks chasing,time survived and progress bar for special boost

---
+++ Assets/Scripts/BabyBatController.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BabyBatController : MonoBehaviour {
+
+    private Transform _target;
+    private Transform _cave;
+    public float moveSpeed;
+    private int rotationSpeed = 50;
+    private BatFlapController batFlapController;
+    public GameObject BabyBat;
+    private string _follow;
+    public float followParentDistance;
+    public Text babystatusHUD;
+    void Start()
+    {
+        _target = GameObject.FindWithTag("player").transform;
+        _cave = GameObject.FindWithTag("cave").transform;
+        batFlapController = BabyBat.GetComponent<BatFlapController>();
+        _follow = "";
+        babystatusHUD.text = "Baby Status : Not Found";
+    }
+
+
+    void Update()
+    {
+        Move();
+    }
+
+    void FollowParent()
+    {
+        if (_follow.Equals("bat"))
+        {
+            //rotate to look at the player
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+            Quaternion.LookRotation(_target.position - transform.position), rotationSpeed * Time.deltaTime);
+
+            //move towards the player
+            GetComponent<Rigidbody>().AddForce(transform.forward * moveSpeed * Time.deltaTime);
+            batFlapController.FlapWings();
+            babystatusHUD.text = "Baby Status : Found";
+        }
+        //Only set it the first time
+        else if (Vector3.Distance(transform.position, _target.position) <= followParentDistance)
+        {
+            transform.Find("Bat_morph/Bat_Anim0").gameObject.SetActive(false);
+            _follow = "bat";
+            babystatusHUD.text = "Baby Status : Not Found";
+        }
+    }
+
+    void EnterCave()
+    {
+        if (_follow.Equals("cave"))
+        {
+            //rotate to look at the player
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+            Quaternion.LookRotation(_cave.position - transform.position), rotationSpeed * Time.deltaTime);
+
+            //move towards the player
+            GetComponent<Rigidbody>().AddForce(transform.forward * moveSpeed * Time.deltaTime);
+            batFlapController.FlapWings();
+        }
+        //Only set it the first time
+        else if(Vector3.Distance(transform.position, _cave.position) <= followParentDistance)
+        {
+            _follow = "cave";
+        }
+    }
+
+    void Move()
+    {
+        EnterCave();
+        if (!_follow.Equals("cave"))
+        {
+            FollowParent();
+        }
+    }
+}
---
+++ Assets/Scripts/HawkTerritory.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class HawkTerritory : MonoBehaviour {
+    public Text hawksFollowingHUD;
+    // Use this for initialization
+    void Start () {
+        hawksFollowingHUD.text = "Hawks Following : NO";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            transform.Find("Hawk").GetComponent<HawkController>().attackingBat = true;
+            hawksFollowingHUD.text = "Hawks Following : YES";
+        } 
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            transform.Find("Hawk").GetComponent<HawkController>().attackingBat = false;
+            hawksFollowingHUD.text = "Hawks Following : NO";
+        }
+    }
+}

970a5c004864734395703ca1968e7ae1bfdb5e66
Audrey <audreyjw@hotmail.com>
2016-12-01 15:15:18 -0500

    cinematics for tutorial 1, tutorial 2, level 1-2-3!

---
+++ Assets/Scripts/callBackCameraCinematic.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class callBackCameraCinematic : MonoBehaviour {
+
+    private Cinematics cinematicScriptCallBack; 
+
+    public void callBack()
+    {
+        cinematicScriptCallBack = GameObject.Find("Camera").GetComponent<Cinematics>();
+        cinematicScriptCallBack.switchCamera(); 
+    }
+}
